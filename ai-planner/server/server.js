bb6cc9bb94be67f6bcb882797a9c2c61f2052c55
Oburjin <140142233+Oburjin@users.noreply.github.com>
2026-03-10 01:41:10 +1100

    Add server endpoint to fetch gemini responses for plans

---
+++ ai-planner/server/server.js
@@ -0,0 +1,63 @@
+import express from "express";
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+    model: "gemini-2.5-flash"
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/test-ai", async (req, res) => {
+    const result = await model.generateContent("Say hello");
+    const response = await result.response;
+    res.send(response.text());
+});
+
+app.post("/plan-goal", async function (req, res) {
+    const goal = req.body.goal;
+
+    const prompt = `
+        Break the following goal into tasks.
+
+        Estimate the hours for each task.
+
+        Return ONLY JSON.
+
+        Goal: ${goal}
+
+        Format:
+        {
+        "tasks":[
+        {"task":"task name","hours":number}
+        ]
+        }
+        `;
+
+    const result = await model.generateContent(prompt);
+
+    let text = result.response.text(); 
+    text = text.replace(/```json/g, "").replace(/```/g, "");
+    const plan = JSON.parse(text);
+    res.json(plan);
+});
+
+app.post("/test-goal", function(req, res) {
+    const goal = req.body.goal;
+
+    res.json({
+        message: "Goal received",
+        goal: goal
+    });
+});
+
+// app.get("/", function(req, res) {
+//     res.send("Server working!");
+// });
+
+app.listen(3000);
