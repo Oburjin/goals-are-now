2ca150e184da22d2b9992f6e73789b63674c421e
Oburjin <140142233+Oburjin@users.noreply.github.com>
2026-03-10 14:50:12 +1100

    Switch AI model due to RPD limits

--- ai-planner/server/server.js
+++ ai-planner/server/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash"
+    // model: "gemini-2.5-flash"
+    model: "gemma-3-1b-it"
+    // model: "gemma-3-12b-it"
 });
 
 const app = express();
